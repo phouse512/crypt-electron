53311c3c3adbb247c3fdeb38f7410db5b1f59751
phouse512 <philiphouse2015@u.northwestern.edu>
2019-11-03 19:42:20 -0600

    checking in work

--- src/renderer/components/modals/PhotoViewModal.jsx
+++ src/renderer/components/modals/PhotoViewModal.jsx
@@ -6,12 +6,30 @@ const PhotoViewModal = ({
   closeHandler,
   currentItem,
   isOpen,
+  nextItemId,
   openHandler,
+  previousItemId,
 }) => {
   let itemView;
   if (currentItem) {
     itemView = (
       <div className="photo-view">
+        <div 
+          className="cancel-overlay"
+          onClick={closeHandler}
+        >
+          <i className="fas fa-times"></i>
+        </div>
+        <div className="photo-view__change next">
+          <div>
+            <i className="fas fa-arrow-right"></i>
+          </div>
+        </div>
+        <div className="photo-view__change previous">
+          <div>
+            <i className="fas fa-arrow-left"></i>
+          </div>
+        </div>
         <div className="photo-view__image">
           <div>
             <img src={currentItem.itemPath} />
@@ -36,16 +54,16 @@ const PhotoViewModal = ({
       {itemView}
     </ReactModal>
   );
-
 }
-
 
 PhotoViewModal.defaultProps = {};
 PhotoViewModal.PropTypes = {
   closeHandler: PropTypes.func.isRequired,
   currentItem: PropTypes.shape({}).isRequired,
   isOpen: PropTypes.bool.isRequired,
+  nextItemId: PropTypes.number.isRequired,
   openHandler: PropTypes.func.isRequired,
+  previousItemId: PropTypes.number.isRequired,
 };
 
 export default PhotoViewModal;
